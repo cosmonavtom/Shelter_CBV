5225557a8b395a5ca2156e883e5bd4f268d5ccf1
cosmonavtom <bedbmakc@gmail.com>
2024-10-15 15:31:39 +0300

    Изменено имя модели, регистрация моделей Category и Dog, миграция по изменению имени модели

--- dogs/admin.py
+++ dogs/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from dogs.models import Dog, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name')
+    ordering = ('pk', 'name')
+
+
+@admin.register(Dog)
+class Dog(admin.ModelAdmin):
+    list_display = ('name', 'category')
+    list_filter = ('category',)
+    ordering = ('name',)
