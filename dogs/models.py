183102ac2dcb8b36facf85112e0a025d4c2241f0
cosmonavtom <bedbmakc@gmail.com>
2024-10-15 14:28:47 +0300

    Добавлена модель Dogs, миграции произведены

--- dogs/models.py
+++ dogs/models.py
@@ -12,3 +12,16 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'breed'
         verbose_name_plural = 'breeds'
+
+class Dogs(models.Model):
+    name = models.CharField(max_length=250, verbose_name='dog_name')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='breed')
+    photo = models.ImageField(upload_to='dogs/', **NULLABLE, verbose_name='image')
+    birth_date = models.DateTimeField(**NULLABLE, verbose_name='birth_date')
+
+    def __str__(self):
+        return f'{self.name} ({self.category})'
+
+    class Meta:
+        verbose_name = 'dog'
+        verbose_name_plural = 'dogs'
