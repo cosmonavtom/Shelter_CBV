8cc22fc67014a19e34e18c4985c9f6ed31ab043b
cosmonavtom <bedbmakc@gmail.com>
2024-10-25 13:18:56 +0300

    Добавлен StyleFormMixin

--- users/forms.py
+++ users/forms.py
@@ -3,13 +3,20 @@ from django import forms
 from users.models import User
 
 
-class UserForm(forms.ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class UserForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name', 'phone',)
 
 
-class UserRegisterForm(forms.ModelForm):
+class UserRegisterForm(StyleFormMixin, forms.ModelForm):
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
 
@@ -24,6 +31,8 @@ class UserRegisterForm(forms.ModelForm):
         return cd['password2']
 
 
-class UserLoginForm(forms.Form):
+class UserLoginForm(StyleFormMixin, forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
