348eaf38faaab15efa50886bb2642401d5b8b2de
cosmonavtom <bedbmakc@gmail.com>
2024-10-30 18:03:49 +0300

    Добавлена: user_generate_new_password

--- users/views.py
+++ users/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render, reverse, redirect
+import random
+import string
+
 from django.http import HttpResponseRedirect, HttpResponse
+
+from django.shortcuts import render, reverse, redirect
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 from users.forms import UserRegisterForm, UserLoginForm, UserUpdateForm, UserPasswordChangeForm
+from users.services import send_register_email, send_new_password
 
 
 def user_register_view(request):
@@ -14,6 +19,7 @@ def user_register_view(request):
             new_user = form.save()
             new_user.set_password(form.cleaned_data['password'])
             new_user.save()
+            send_register_email(new_user.email)
             return HttpResponseRedirect(reverse('users:login_user'))
     context = {
         'form': form
@@ -33,6 +39,7 @@ def user_login_view(request):
                     return HttpResponseRedirect(reverse('dogs:index'))
                 else:
                     return HttpResponse('Аккаунт неактивен')
+
     form = UserLoginForm()
     context = {
         'form': form
@@ -92,3 +99,29 @@ def user_change_password_view(request):
 def user_logout_view(request):
     logout(request)
     return redirect('dogs:index')
+
+
+@login_required
+def user_generate_new_password(request):
+    new_password = ''.join(random.sample((string.ascii_letters + string.digits), 12))
+    request.user.set_password(new_password)
+    request.user.save()
+    send_new_password(request.user.email, new_password)
+    return redirect(reverse('dogs:index'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
