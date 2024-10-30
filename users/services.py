b2c93ef5b0a87117707147db144e9d04336b2fc9
cosmonavtom <bedbmakc@gmail.com>
2024-10-30 18:04:30 +0300

    Добавлены сервисы почты

---
+++ users/services.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.core.mail import send_mail
+
+
+def send_register_email(email):
+    send_mail(
+        subject='Поздравляю с регистрацией',
+        message='Вы успешно зарегистрировались! Добро пожаловать!',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[email]
+    )
+
+
+def send_new_password(email, new_password):
+    send_mail(
+        subject='Вы успешно изменили пароль',
+        message=f'Ваш новый пароль: {new_password}',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[email]
+    )
