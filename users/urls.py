7ceb6399781a34c9e7c7ba2e01be4da4abe56378
cosmonavtom <bedbmakc@gmail.com>
2024-10-30 18:05:59 +0300

    Добавлены урлы

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from users.apps import UsersConfig
 from users.views import (user_register_view, user_login_view, user_profile_view, user_logout_view, user_update_view,
-                         user_change_password_view)
+                         user_change_password_view, user_generate_new_password)
 
 app_name = UsersConfig.name
 
@@ -12,5 +12,6 @@ urlpatterns = [
     path('register/', user_register_view, name='register_user'),
     path('profile/', user_profile_view, name='profile_user'),
     path('update/', user_update_view, name='update_user'),
-    path('change_password/', user_change_password_view, name='change_password_user')
+    path('change_password/', user_change_password_view, name='change_password_user'),
+    path('profile/genpassword/', user_generate_new_password, name='user_generate_new_password')
 ]
