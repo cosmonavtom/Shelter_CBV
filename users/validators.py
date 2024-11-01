6f1b63f8862c4826809297c70df5f78ef4629c81
cosmonavtom <bedbmakc@gmail.com>
2024-11-01 19:53:49 +0300

    Добавлена многоязычность для валидатора

--- users/validators.py
+++ users/validators.py
@@ -1,12 +1,31 @@
 import re
+
+from django.conf import  settings
 from django.core.exceptions import ValidationError
 
 
 def validate_password(field):
     pattern = re.compile(r'^[A-Za-z0-9]+$')
+    language = settings.LANGUAGE_CODE
+    error_messages = [
+        {
+            'ru-ru': 'Только латиница и цифры',
+            'en-us': 'Must contain A-Z, a-z letters and 0-9 numbers'
+        },
+        {
+            'ru-ru': 'Длина пароля от 6 до 12 символов',
+            'en-us': 'Password length must be between 6 and 12'
+        }
+    ]
     if not bool(re.match(pattern, field)):
-        # print('Must contain A-Z, a-z letters and 0-9 numbers')
-        raise ValidationError('Только латиница и цифры')
+        # print(error_messages[0][language])
+        raise ValidationError(
+            error_messages[0][language],
+            code=error_messages[0][language]
+        )
     if not 6 <= len(field) <= 12:
-        # print('Password length must be between 6 and 12')
-        raise ValidationError('Длина пароля от 6 до 12 символов')
+        # print(error_messages[1][language])
+        raise ValidationError(
+            error_messages[1][language],
+            code=error_messages[1][language]
+        )
