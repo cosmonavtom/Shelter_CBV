786d211b97ce3c648f599ddb34d72e5d3ba66a36
cosmonavtom <bedbmakc@gmail.com>
2024-11-05 20:01:26 +0300

    Добавили redis и кешируем Главную и Породы

--- config/settings.py
+++ config/settings.py
@@ -155,6 +155,15 @@ LOGIN_REDIRECT_URL = 'dogs:index'  # '/'
 # LOGOUT_REDIRECT_URL = 'dogs:index'
 LOGIN_URL = '/users/'
 
+CACHE_ENABLED = os.getenv('CACHE_ENABLED') == 'True'
+if CACHE_ENABLED:
+    CACHES = {
+        'default': {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": os.getenv("CACHE_LOCATION")
+        }
+    }
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.com'
 EMAIL_PORT = 465
