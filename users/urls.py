81e5087f3aa24f9f79f921b361db7d7144fda16a
cosmonavtom <bedbmakc@gmail.com>
2024-11-14 17:17:06 +0300

    Добавлена возможность просмотра списка пользователей

--- users/urls.py
+++ users/urls.py
@@ -1,17 +1,22 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import user_generate_new_password, \
-    UserRegisterView, UserLoginView, UserProfileView, UserUpdateView, UserPasswordChangeView, UserLogoutView
+from users.views import user_generate_new_password, UserRegisterView, UserLoginView, UserProfileView, \
+    UserUpdateView, UserPasswordChangeView, UserLogoutView, UserListView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
+    # работа с аккаунтом
     path('', UserLoginView.as_view(), name='login_user'),
     path('logout/', UserLogoutView.as_view(), name='logout_user'),
     path('register/', UserRegisterView.as_view(), name='register_user'),
     path('profile/', UserProfileView.as_view(), name='profile_user'),
     path('update/', UserUpdateView.as_view(), name='update_user'),
     path('change_password/', UserPasswordChangeView.as_view(), name='change_password_user'),
-    path('profile/genpassword/', user_generate_new_password, name='user_generate_new_password')
+    path('profile/genpassword/', user_generate_new_password, name='user_generate_new_password'),
+
+    # просмотр других пользователей
+    path('all_users/', UserListView.as_view(), name='users_list'),
+
 ]
