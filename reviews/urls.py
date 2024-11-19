e4b2a720c07115ca336c847adb101daf7e33f6c8
cosmonavtom <bedbmakc@gmail.com>
2024-11-19 15:43:45 +0300

    Добавлены шаблоны для отзывов

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
 from reviews.apps import ReviewsConfig
-from reviews.views import DogReviewListView, DeactivatedDogReviewListView
+from reviews.views import ReviewListView, DeactivatedDogReviewListView
 
 app_name = ReviewsConfig.name
 
 urlpatterns = [
-    path('<int:pk>/reviews/', DogReviewListView.as_view(), name='list_reviews'),
-    path('<int:pk>/reviews/deactivated/', DeactivatedDogReviewListView.as_view(), name='deactivated_reviews_list'),
+    path('',ReviewListView.as_view(), name='list_reviews'),
+    path('deactivated/', DeactivatedDogReviewListView.as_view(), name='deactivated_reviews_list'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render, reverse, redirect, get_object_or_404
-from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.shortcuts import render
+from django.http import HttpResponseForbidden
+from django.shortcuts import reverse, get_object_or_404, redirect
+from django.views.generic import CreateView, CreateView, DeleteView, UpdateView, ListView
+from django.core.exceptions import PermissionDenied
 
 from reviews.models import Review
 from users.models import UserRoles
 from reviews.forms import ReviewForm
 
 
-class DogReviewListView(LoginRequiredMixin, ListView):
+class ReviewListView(LoginRequiredMixin, ListView):
     model = Review
     extra_context = {
-        'title': 'Все отзывы о собаке'
+        'title': 'Питомник - Все отзывы по собаке'
     }
     template_name = 'reviews/reviews.html'
 
@@ -30,11 +33,55 @@ class DeactivatedDogReviewListView(LoginRequiredMixin, ListView):
     template_name = 'reviews/reviews.html'
 
     def get_queryset(self):
-        queryset = super().get_queryset()
-        # queryset = queryset.filter(dog_pk=self.kwargs.get('pk'))
+        queryset = super().queryset()
+        queryset = queryset.filter(dog_pk=self.kwargs.get('pk'))
         queryset = queryset.filter(sign_of_review=False)
 
         return queryset
 
 
+class ReviewCreateView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = 'reviews/review_create_review.html'
 
+
+class ReviewDetailView(LoginRequiredMixin, DeleteView):
+    model = Review
+    template_name = 'reviews/review_detail.html'
+
+
+class ReviewUpdateView(LoginRequiredMixin, DeleteView):
+    model = Review
+    form_class = ReviewForm
+    template_name = 'reviews/review_create_update.html'
+
+    def get_success_url(self):
+        return reverse('reviews:detail_review', args=[self.kwargs.get('slug')])
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        if self.object.autor != self.request.user and self.request.user not in [UserRoles.ADMIN, UserRoles.MODERATOR]:
+            raise PermissionDenied()
+        return self.object
+
+
+class ReviewDeleteView(PermissionRequiredMixin, DeleteView):
+    model = Review
+    template_name = 'reviews/review_delete.html'
+    permission_required = 'reviews.delete_review'
+
+    def get_success_url(self):
+        return reverse('reviews:list_reviews')
+
+
+def review_toggle_activity(request, sjug):
+    review_item = get_object_or_404(Review, slug=sjug)
+    if review_item.sign_of_review:
+        review_item.sign_of_review = False
+        review_item.save()
+        return redirect(reverse('reviews:deactivated_reviews'))
+    else:
+        review_item.sign_of_review = True
+        review_item.save()
+        return redirect(reverse('reviews:list_reviews'))
