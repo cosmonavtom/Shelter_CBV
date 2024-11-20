3477d56a6e17c7f56482330d205630ac6573729b
cosmonavtom <bedbmakc@gmail.com>
2024-11-20 14:10:55 +0300

    Добавлена утилита для случайного slug и встроена во views

--- reviews/views.py
+++ reviews/views.py
@@ -8,6 +8,7 @@ from django.core.exceptions import PermissionDenied
 from reviews.models import Review
 from users.models import UserRoles
 from reviews.forms import ReviewForm
+from reviews.utils import slug_generator
 
 
 class ReviewListView(LoginRequiredMixin, ListView):
@@ -15,26 +16,24 @@ class ReviewListView(LoginRequiredMixin, ListView):
     extra_context = {
         'title': 'Питомник - Все отзывы по собаке'
     }
-    template_name = 'reviews/reviews.html'
+    template_name = 'reviews/reviews_list.html'
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        # queryset = queryset.filter(dog_pk=self.kwargs.get('pk'))
         queryset = queryset.filter(sign_of_review=True)
 
         return queryset
 
 
-class DeactivatedReviewListView(LoginRequiredMixin, ListView):
+class ReviewDeactivatedListView(LoginRequiredMixin, ListView):
     model = Review
     extra_context = {
         'title': 'Неактивные отзывы'
     }
-    template_name = 'reviews/reviews.html'
+    template_name = 'reviews/reviews_list.html'
 
     def get_queryset(self):
-        queryset = super().queryset()
-        queryset = queryset.filter(dog_pk=self.kwargs.get('pk'))
+        queryset = super().get_queryset()
         queryset = queryset.filter(sign_of_review=False)
 
         return queryset
@@ -44,6 +43,18 @@ class ReviewCreateView(LoginRequiredMixin, CreateView):
     model = Review
     form_class = ReviewForm
     template_name = 'reviews/review_create_update.html'
+
+    def form_valid(self, form):
+        if self.request.user.role not in [UserRoles.USER, UserRoles.ADMIN]:
+            return HttpResponseForbidden()
+        self.object = form.save()
+        print(self.object.slug)
+        if self.object.slug == 'temp_slug':
+            self.object.slug = slug_generator()
+            print(self.object.slug)
+        self.object.autor = self.request.user
+        self.object.save()
+        return super().form_valid(form)
 
 
 class ReviewDetailView(LoginRequiredMixin, DetailView):
@@ -61,7 +72,7 @@ class ReviewUpdateView(LoginRequiredMixin, UpdateView):
 
     def get_object(self, queryset=None):
         self.object = super().get_object(queryset)
-        if self.object.autor != self.request.user and self.request.user not in [UserRoles.ADMIN, UserRoles.MODERATOR]:
+        if self.object.author != self.request.user and self.request.user not in [UserRoles.ADMIN, UserRoles.MODERATOR]:
             raise PermissionDenied()
         return self.object
 
