eb621ee38bb5aeb67d55726025ef5d30d01b3cc6
cosmonavtom <bedbmakc@gmail.com>
2024-11-20 14:12:44 +0300

    Созданы формы для CRUD reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 
 from reviews.apps import ReviewsConfig
 
-from reviews.views import ReviewListView, DeactivatedReviewListView, ReviewCreateView, ReviewDetailView, \
+from reviews.views import ReviewListView, ReviewDeactivatedListView, ReviewCreateView, ReviewDetailView, \
     ReviewUpdateView, ReviewDeleteView, review_toggle_activity
 
 app_name = ReviewsConfig.name
 
 urlpatterns = [
     path('', ReviewListView.as_view(), name='list_reviews'),
-    path('deactivated/', DeactivatedReviewListView.as_view(), name='deactivated_reviews'),
+    path('deactivated/', ReviewDeactivatedListView.as_view(), name='deactivated_reviews'),
     path('review/create/', ReviewCreateView.as_view(), name='create_review'),
     path('review/detail/<slug:slug>/', ReviewDetailView.as_view(), name='detail_review'),
     path('review/update/<slug:slug>', ReviewUpdateView.as_view(), name='update_review'),
