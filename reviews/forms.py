63d52c0920c597395a22811295e6514d4a6ad63a
cosmonavtom <bedbmakc@gmail.com>
2024-11-21 18:26:30 +0300

    поправлены формы

--- reviews/forms.py
+++ reviews/forms.py
@@ -7,7 +7,7 @@ from dogs.forms import StyleFormMixin
 class ReviewForm(StyleFormMixin, forms.ModelForm):
     title = forms.CharField(max_length=150, label='Заголовок_формы')
     content = forms.TextInput()
-    slug = forms.SlugField(max_length=10, initial='temp_slug', widget=forms.HiddenInput())
+    slug = forms.SlugField(max_length=20, initial='temp_slug', widget=forms.HiddenInput())
 
     class Meta:
         model = Review
