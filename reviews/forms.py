1d9c1ba15dda5050dfeb64037728619d2bc9aa0f
cosmonavtom <bedbmakc@gmail.com>
2024-11-27 15:51:59 +0300

    Докстринги для reviews/forms

--- reviews/forms.py
+++ reviews/forms.py
@@ -5,6 +5,7 @@ from dogs.forms import StyleFormMixin
 
 
 class ReviewForm(StyleFormMixin, forms.ModelForm):
+    ''' Форма для отзывов с указанными полями. Используется slug '''
     title = forms.CharField(max_length=150, label='Заголовок_формы')
     content = forms.TextInput()
     slug = forms.SlugField(max_length=20, initial='temp_slug', widget=forms.HiddenInput())
