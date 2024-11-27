59030b84fc17b8cf7a2f4fc5017ba93be46be744
cosmonavtom <bedbmakc@gmail.com>
2024-11-27 15:38:05 +0300

    Докстринги для dogs/admin

--- dogs/admin.py
+++ dogs/admin.py
@@ -5,12 +5,14 @@ from dogs.models import Dog, Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    ''' Породы с полями и сортировкой для админки'''
     list_display = ('pk', 'name')
     ordering = ('pk', 'name')
 
 
 @admin.register(Dog)
 class Dog(admin.ModelAdmin):
+    ''' Собаки с полями и сортировкой для админки '''
     list_display = ('name', 'category', 'is_active')
     list_filter = ('category',)
     ordering = ('name',)
