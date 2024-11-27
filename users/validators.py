40c0a414d331054d18f6505054739cba7b60abe7
cosmonavtom <bedbmakc@gmail.com>
2024-11-27 17:27:08 +0300

    Докстринги для users/validators

--- users/validators.py
+++ users/validators.py
@@ -5,6 +5,7 @@ from django.core.exceptions import ValidationError
 
 
 def validate_password(field):
+    ''' Создание паттерна для пароля и проверка на его соответствие '''
     pattern = re.compile(r'^[A-Za-z0-9]+$')
     language = settings.LANGUAGE_CODE
     error_messages = [
